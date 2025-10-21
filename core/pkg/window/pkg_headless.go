b0150a7866cf6b04452736a96208f331b9ef751a
vistor <vistormu@gmail.com>
2025-10-21 18:49:12 +0200

    headless build

--- core/pkg/window/pkg_headless.go
+++ core/pkg/window/pkg_headless.go
@@ -2,11 +2,11 @@
 
 package window
 
-type Window any
-
 import (
 	"github.com/vistormu/xpeto/core/ecs"
 	"github.com/vistormu/xpeto/core/schedule"
 )
+
+type Window struct{}
 
 func Pkg(w *ecs.World, sch *schedule.Scheduler) {}
